5eaf397a0d3088d82a4a24a0c5f62105bb026e20
limingpu <85180586@qq.com>
2023-11-18 12:27:55 +0800

    2

--- blog-springboot-mp/src/main/java/com/example/blog/model/vo/UserInfoVO.java
+++ blog-springboot-mp/src/main/java/com/example/blog/model/vo/UserInfoVO.java
@@ -13,6 +13,6 @@ import java.io.Serializable;
 public class UserInfoVO extends UserInfo implements Serializable {
     @Serial
     private static final long serialVersionUID = 1660890311645535668L;
-    private String checkCode; //二次输入检查和第一次输入的密码是否相同
+    private String checkCode; // 输入的验证码
     private String codeKey; //验证码
 }
